54316a8a83b7dca890271f56d0e6d37887598e3d
Admin <Admin@IPASON-WINDOWS>
2022-09-09 19:39:47 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Core;
+using Microsoft.Toolkit.Uwp.Notifications;
+using WinUI3.sources.Pages;
+using Windows.Storage;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUI3
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+
+            this.InitializeComponent();
+            new ToastContentBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddArgument("conversationId", 9813)
+                .AddText("您已經成功啓動“神奇的應用程式”！")
+                .AddText("您可以盡情使用此應用！")
+                .Show();
+
+            Windows.Storage.ApplicationDataContainer localSettings =
+                Windows.Storage.ApplicationData.Current.LocalSettings;
+            Windows.Storage.StorageFolder localFolder =
+                Windows.Storage.ApplicationData.Current.LocalFolder;
+
+            async void WriteTimestamp()
+            {
+                Windows.Globalization.DateTimeFormatting.DateTimeFormatter formatter =
+                    new Windows.Globalization.DateTimeFormatting.DateTimeFormatter("longtime");
+
+                StorageFile sampleFile = await localFolder.CreateFileAsync("dataFile.txt",
+                    CreationCollisionOption.ReplaceExisting);
+                await FileIO.WriteTextAsync(sampleFile, formatter.Format(DateTimeOffset.Now));
+            }
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            FrameNavigationOptions navOptions = new FrameNavigationOptions();
+            navOptions.TransitionInfoOverride = args.RecommendedNavigationTransitionInfo;
+            if (args.InvokedItemContainer == home)
+            {
+                ContentFrame.Navigate(typeof(HomePage), null);
+                NavView.Header = "主页";
+            }
+            else if (args.IsSettingsInvoked)
+            {
+                ContentFrame.Navigate(typeof(SettingsPage), null);
+                NavView.Header = "设置";
+            }
+            else if (args.InvokedItemContainer == Launcher)
+            {
+                ContentFrame.Navigate(typeof(LauncherPage), null, new DrillInNavigationTransitionInfo());
+                NavView.Header = "启动器";
+            }
+            else if (args.InvokedItemContainer == Pictures)
+            {
+                ContentFrame.Navigate(typeof(PicturesPage), null, new DrillInNavigationTransitionInfo());
+                NavView.Header = "每日一图";
+            }
+            else if (args.InvokedItemContainer == Music)
+            {
+                ContentFrame.Navigate(typeof(MusicPage), null, new DrillInNavigationTransitionInfo());
+                NavView.Header = "音乐";
+            }
+            else if (args.InvokedItemContainer == Search)
+            {
+                ContentFrame.Navigate(typeof(SearchPage), null, new DrillInNavigationTransitionInfo());
+                NavView.Header = "搜索";
+            }
+        }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(HomePage), null);
+        }
+    }
+}
---
+++ sources/Pages/MusicPage/MusicPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUI3.sources.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MusicPage : Page
+    {
+        public MusicPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            string url = "https://music.163.com/#/search/m/?s=" + SearchBox.Text;
+            Uri targetUri = new Uri(url);
+            explorer.Source = targetUri;
+        }
+
+        private void Sound_Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
