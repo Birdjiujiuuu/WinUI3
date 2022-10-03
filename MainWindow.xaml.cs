09c62a37d635eb95cfb03fd6190f43d127f3c4d1
鸿彬 刘 <h2097571086@outlook.com>
2022-10-03 12:45:32 +0800

    标题栏更新

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,6 +18,10 @@ using Windows.UI.Core;
 using Microsoft.Toolkit.Uwp.Notifications;
 using WinUI3.sources.Pages;
 using Windows.Storage;
+using Microsoft.UI.Windowing;
+using Microsoft.UI;
+using System.Runtime.InteropServices;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -29,11 +33,35 @@ namespace WinUI3
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private AppWindow m_AppWindow;
+
         public MainWindow()
         {
-
-
             this.InitializeComponent();
+
+            m_AppWindow = GetAppWindowForCurrentWindow();
+            m_AppWindow.Changed += AppWindow_Changed;
+
+            // Check to see if customization is supported.
+            // Currently only supported on Windows 11.
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                var titleBar = m_AppWindow.TitleBar;
+                titleBar.ExtendsContentIntoTitleBar = true;
+                AppTitleBar.Loaded += AppTitleBar_Loaded;
+                AppTitleBar.SizeChanged += AppTitleBar_SizeChanged;
+            }
+            else
+            {
+                // Title bar customization using these APIs is currently
+                // supported only on Windows 11. In other cases, hide
+                // the custom title bar element.
+                AppTitleBar.Visibility = Visibility.Collapsed;
+
+                // Show alternative UI for any functionality in
+                // the title bar, such as search.
+            }
+
             new ToastContentBuilder()
                 .AddArgument("action", "viewConversation")
                 .AddArgument("conversationId", 9813)
@@ -45,15 +73,132 @@ namespace WinUI3
                 Windows.Storage.ApplicationData.Current.LocalSettings;
             Windows.Storage.StorageFolder localFolder =
                 Windows.Storage.ApplicationData.Current.LocalFolder;
+        }
 
-            async void WriteTimestamp()
+        private void AppTitleBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (AppWindowTitleBar.IsCustomizationSupported())
             {
-                Windows.Globalization.DateTimeFormatting.DateTimeFormatter formatter =
-                    new Windows.Globalization.DateTimeFormatting.DateTimeFormatter("longtime");
+                SetDragRegionForCustomTitleBar(m_AppWindow);
+            }
+        }
 
-                StorageFile sampleFile = await localFolder.CreateFileAsync("dataFile.txt",
-                    CreationCollisionOption.ReplaceExisting);
-                await FileIO.WriteTextAsync(sampleFile, formatter.Format(DateTimeOffset.Now));
+        private void AppTitleBar_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (AppWindowTitleBar.IsCustomizationSupported()
+                && m_AppWindow.TitleBar.ExtendsContentIntoTitleBar)
+            {
+                // Update drag region if the size of the title bar changes.
+                SetDragRegionForCustomTitleBar(m_AppWindow);
+            }
+        }
+
+        private AppWindow GetAppWindowForCurrentWindow()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(wndId);
+        }
+
+        [DllImport("Shcore.dll", SetLastError = true)]
+        internal static extern int GetDpiForMonitor(IntPtr hmonitor, Monitor_DPI_Type dpiType, out uint dpiX, out uint dpiY);
+
+        internal enum Monitor_DPI_Type : int
+        {
+            MDT_Effective_DPI = 0,
+            MDT_Angular_DPI = 1,
+            MDT_Raw_DPI = 2,
+            MDT_Default = MDT_Effective_DPI
+        }
+
+        private double GetScaleAdjustment()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            DisplayArea displayArea = DisplayArea.GetFromWindowId(wndId, DisplayAreaFallback.Primary);
+            IntPtr hMonitor = Win32Interop.GetMonitorFromDisplayId(displayArea.DisplayId);
+
+            // Get DPI.
+            int result = GetDpiForMonitor(hMonitor, Monitor_DPI_Type.MDT_Default, out uint dpiX, out uint _);
+            if (result != 0)
+            {
+                throw new Exception("Could not get DPI for monitor.");
+            }
+
+            uint scaleFactorPercent = (uint)(((long)dpiX * 100 + (96 >> 1)) / 96);
+            return scaleFactorPercent / 100.0;
+        }
+
+        private void SetDragRegionForCustomTitleBar(AppWindow appWindow)
+        {
+            if (AppWindowTitleBar.IsCustomizationSupported()
+                && appWindow.TitleBar.ExtendsContentIntoTitleBar)
+            {
+                double scaleAdjustment = GetScaleAdjustment();
+
+                List<Windows.Graphics.RectInt32> dragRectsList = new();
+
+                Windows.Graphics.RectInt32 dragRectL;
+                dragRectL.X = (int)((LeftPaddingColumn.ActualWidth) * scaleAdjustment);
+                dragRectL.Y = 0;
+                dragRectL.Height = (int)(AppTitleBar.ActualHeight * scaleAdjustment);
+                dragRectL.Width = (int)((IconColumn.ActualWidth
+                                        + TitleColumn.ActualWidth
+                                        + LeftDragColumn.ActualWidth) * scaleAdjustment);
+                dragRectsList.Add(dragRectL);
+
+                Windows.Graphics.RectInt32 dragRectR;
+                dragRectR.X = (int)((LeftPaddingColumn.ActualWidth
+                                    + IconColumn.ActualWidth
+                                    + RightPaddingColumn.ActualWidth
+                                    + TitleTextBlock.ActualWidth
+                                    + LeftDragColumn.ActualWidth
+                                    + SearchColumn.ActualWidth) * scaleAdjustment);
+                dragRectR.Y = 0;
+                dragRectR.Height = (int)(AppTitleBar.ActualHeight * scaleAdjustment);
+                dragRectR.Width = (int)(RightDragColumn.ActualWidth * scaleAdjustment);
+                dragRectsList.Add(dragRectR);
+
+                Windows.Graphics.RectInt32[] dragRects = dragRectsList.ToArray();
+
+                appWindow.TitleBar.SetDragRectangles(dragRects);
+            }
+        }
+
+        private void AppWindow_Changed(AppWindow sender, AppWindowChangedEventArgs args)
+        {
+            if (args.DidPresenterChange
+                && AppWindowTitleBar.IsCustomizationSupported())
+            {
+                switch (sender.Presenter.Kind)
+                {
+                    case AppWindowPresenterKind.CompactOverlay:
+                        // Compact overlay - hide custom title bar
+                        // and use the default system title bar instead.
+                        AppTitleBar.Visibility = Visibility.Collapsed;
+                        sender.TitleBar.ResetToDefault();
+                        break;
+
+                    case AppWindowPresenterKind.FullScreen:
+                        // Full screen - hide the custom title bar
+                        // and the default system title bar.
+                        AppTitleBar.Visibility = Visibility.Collapsed;
+                        sender.TitleBar.ExtendsContentIntoTitleBar = true;
+                        break;
+
+                    case AppWindowPresenterKind.Overlapped:
+                        // Normal - hide the system title bar
+                        // and use the custom title bar instead.
+                        AppTitleBar.Visibility = Visibility.Visible;
+                        sender.TitleBar.ExtendsContentIntoTitleBar = true;
+                        SetDragRegionForCustomTitleBar(sender);
+                        break;
+
+                    default:
+                        // Use the default system title bar.
+                        sender.TitleBar.ResetToDefault();
+                        break;
+                }
             }
         }
 
